e4f56f2236ed798ae85be370c0775d7ceb2d796a
cakebake (Jens A.) <cakebake.dev@gmail.com>
2015-04-15 01:32:47 +0200

    test10LevelsRequireOnce

--- tests/CombinePhpRequireOnceTest.php
+++ tests/CombinePhpRequireOnceTest.php
@@ -24,4 +24,39 @@ class CombinePhpRequireOnceTest extends cakebake\combiner\TestCase
         $this->assertTrue($combine->isParsed('vfs://test1LevelRequireOnce/filename1-level-1.php'));
         $this->assertTrue($combine->isParsed('vfs://test1LevelRequireOnce/filename2-level-1.php'));
     }
+
+    /**
+    * RequireOnce level 10
+    */
+    public function test10LevelsRequireOnce()
+    {
+        $structure = require $this->filesystemDir . '/test10LevelsRequireOnce.php';
+        array_walk_recursive(
+            $structure,
+            array('parent', 'interlaceTree'),
+            [
+                'structure' => $structure,
+                'depthLimit' => 10,
+            ]
+        );
+
+        $this->createFilesystem('test10LevelsRequireOnce', $structure);
+
+        $this->assertFileExists($this->getFilesystemStream('index.php'));
+        $this->assertFileExists($this->getFilesystemStream('filename1-level-1.php'));
+        $this->assertFileExists($this->getFilesystemStream('level-1/filename2-level-2.php'));
+        $this->assertFileExists($this->getFilesystemStream('level-1/level-2/level-3/level-4/filename2-level-5.php'));
+        $this->assertFileExists($this->getFilesystemStream('level-1/level-2/level-3/level-4/level-5/level-6/level-7/level-8/level-9/filename1-level-10.php'));
+
+        $startFile = $this->getFilesystemStream('index.php');
+        $outPath = $this->tmpDir . '/' . self::sanitizeFilename(__METHOD__ . '.php');
+        $combine = PhpFileCombine::init()->parseFile($startFile)->traverse()->prettyPrint(true)->writeFile($outPath);
+
+        $this->assertFileHasNoErrors($outPath);
+        $this->assertTrue($combine->isParsed('vfs://test10LevelsRequireOnce/index.php'));
+        $this->assertTrue($combine->isParsed('vfs://test10LevelsRequireOnce/filename1-level-1.php'));
+        $this->assertTrue($combine->isParsed('vfs://test10LevelsRequireOnce/level-1/filename2-level-2.php'));
+        $this->assertTrue($combine->isParsed('vfs://test10LevelsRequireOnce/level-1/level-2/level-3/level-4/filename2-level-5.php'));
+        $this->assertTrue($combine->isParsed('vfs://test10LevelsRequireOnce/level-1/level-2/level-3/level-4/level-5/level-6/level-7/level-8/level-9/filename1-level-10.php'));
+    }
 }
---
+++ tests/filesystem/test10LevelsRequireOnce.php
@@ -0,0 +1,22 @@
+<?php
+
+//multiple level, recursive copy, simple php
+return [
+    'index.php' => '<?php
+        //start file level-1
+        echo "hello level-1 index!";
+
+        //require_once level-1
+        require_once __DIR__ . "/filename1-level-1.php";
+        require_once __DIR__ . "/filename2-level-1.php";
+
+        //require_once next level from level-1
+        require_once __DIR__ . "/level-1/index.php";
+
+        //end file level-1
+    ',
+    'filename1-level-1.php' => '<?php echo "hello filename1-level-1.php";',
+    'filename2-level-1.php' => '<?php echo "hello filename2-level-1.php";',
+    //dynamic
+    'level-1' => 'SUBDIR',
+];
